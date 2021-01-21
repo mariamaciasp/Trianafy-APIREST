00d27c121b8f08cd5de36acdf6e527c0d207e19e
Maria <mariamaciaspadilla@gmail.com>
2021-01-21 23:31:13 +0100

    empezando con mongo

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ import models from './models';
 import routes from "./routes";
 
 import passport from './service/passport';
+import mongoose from "mongoose";
+
 
 const app = express();
 
@@ -39,8 +41,17 @@ app.use('/user', routes.user);
 app.use('/auth', routes.auth);
 
 
-app.listen(process.env.PORT, () =>
-  console.log(
-    `¡Aplicación de ejemplo escuchando en el puerto ${process.env.PORT}!`
-  )
-);
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true }, err => {
+  
+  if (err) {
+    console.log(`Error de conexión a la base de datos: ${JSON.stringify(err)}`);
+  } else {
+    console.log(`Conexión correcta a la base de datos en la URI ${process.env.DB_URI}`);
+    app.listen(process.env.PORT, () =>
+      console.log(
+        `¡Aplicación de ejemplo escuchando en el puerto ${process.env.PORT}!`
+      )
+    );
+  }
+
+});
